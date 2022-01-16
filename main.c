892650b29b570d6ac81224845d83de0d2d31f034
harshitakumari9 <91043927+harshitakumari9@users.noreply.github.com>
2022-01-16 21:44:37 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
 #include <assert.h>
 
-int numberOfMajorColors =
-    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-
-const int MAX_COLORPAIR_NAME_CHARS = 16;
-int numberOfMinorColors =
-    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-
 int main() {
     testNumberToPair(4, WHITE, BROWN);
     testNumberToPair(5, WHITE, SLATE);
