04a9ccf2aa5f2a87953b59df32ddce721745cac7
harshitakumari9 <91043927+harshitakumari9@users.noreply.github.com>
2022-01-16 21:45:25 +0530

    Update Color_Codes.c

--- Color_Codes.c
+++ Color_Codes.c
@@ -5,6 +5,12 @@ typedef struct {
     enum MinorColor minorColor;
 } ColorPair;
 
+int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+
+const int MAX_COLORPAIR_NAME_CHARS = 16;
+
+int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
 void ColorPairToString(const ColorPair* colorPair, char* buffer) {
     sprintf(buffer, "%s %s",
         MajorColorNames[colorPair->majorColor],
