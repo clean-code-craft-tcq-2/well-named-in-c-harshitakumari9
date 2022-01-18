99701845e1651151260684f4de85700dcd2b07a7
harshitakumari9 <91043927+harshitakumari9@users.noreply.github.com>
2022-01-18 22:08:09 +0530

    Update Color_Codes.c

--- Color_Codes.c
+++ Color_Codes.c
@@ -1,5 +1,8 @@
 #include "Color_Codes.h"
 
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
 typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
