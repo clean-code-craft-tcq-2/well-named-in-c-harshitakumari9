45b2a3f8bac5f9f076163c820718501ac55aee43
harshitakumari9 <91043927+harshitakumari9@users.noreply.github.com>
2022-01-19 14:25:49 +0530

    Update Test_ColorPair.h

--- Test_ColorPair.h
+++ Test_ColorPair.h
@@ -1,2 +1,4 @@
+#include "Color_Codes.h"
+
 extern void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
 extern void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
