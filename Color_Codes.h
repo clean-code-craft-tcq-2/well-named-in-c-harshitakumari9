83b950b75c6e9ea791218d5479e9625cf840c057
harshitakumari9 <91043927+harshitakumari9@users.noreply.github.com>
2022-01-19 11:23:01 +0530

    Update Color_Codes.h

--- Color_Codes.h
+++ Color_Codes.h
@@ -2,5 +2,5 @@ extern const char* MajorColorNames[];
 
 extern const char* MinorColorNames[];
 
-enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+extern enum MajorColor;
+extern enum MinorColor;
