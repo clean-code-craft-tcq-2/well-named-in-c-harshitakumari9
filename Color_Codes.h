71454bb7d29aac935ed9d9a6bf8b9f9d78f82f84
harshitakumari9 <91043927+harshitakumari9@users.noreply.github.com>
2022-01-19 13:45:23 +0530

    Update Color_Codes.h

--- Color_Codes.h
+++ Color_Codes.h
@@ -2,9 +2,6 @@ extern const char* MajorColorNames[];
 
 extern const char* MinorColorNames[];
 
-extern ColorPair GetColorFromPairNumber(int pairNumber);
-extern void ColorPairToString(const ColorPair* colorPair, char* buffer);
-
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
@@ -13,4 +10,5 @@ typedef struct {
     enum MinorColor minorColor;
 } ColorPair;
 
-
+extern ColorPair GetColorFromPairNumber(int pairNumber);
+extern void ColorPairToString(const ColorPair* colorPair, char* buffer);
