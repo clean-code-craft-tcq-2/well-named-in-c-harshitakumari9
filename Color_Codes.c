7d4c0f92b450dc61c2b1e942a482b7486928dd1c
harshitakumari9 <91043927+harshitakumari9@users.noreply.github.com>
2022-01-20 22:29:16 +0530

    Update Color_Codes.c

--- Color_Codes.c
+++ Color_Codes.c
@@ -3,8 +3,10 @@
 
 const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
 const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
 ColorPair GetColorFromPairNumber(int pairNumber);
 void ColorPairToString(const ColorPair* colorPair, char* buffer);
+
 int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
@@ -29,22 +31,13 @@ int GetPairNumberFromColor(const ColorPair* colorPair)
 
 void PrintRefManual()
 {
-    unsigned int choice, pairnum=1;
-    printf("Press 1 to get Reference Manual for Color Codes\n");
-    scanf("%u",&choice);
-    if(choice==1)
-    {
+       unsigned int pairnum=1;
        for(unsigned int majorcolorname=0;majorcolorname<5;majorcolorname++)
-        {
+       {
             for(unsigned int minorcolorname=0;minorcolorname<5;minorcolorname++)
             {
                 printf("%s\t%s\t%u\n", MajorColorNames[majorcolorname],MinorColorNames[minorcolorname],pairnum);
                 pairnum++;
             }
-        } 
-    }
-    else
-    {
-        printf("Invalid Entry !");
-    }
+       } 
 }
